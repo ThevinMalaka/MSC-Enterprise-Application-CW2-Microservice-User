59020c93ca2967efc96dc3b1a6cc18012a3daadf
Thevin Malaka <thevin@orelit.com>
2023-07-21 09:18:00 +0530

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//namespace userService.Data
+//{
+//	public class ApplicationDbContext
+//	{
+//		public ApplicationDbContext()
+//		{
+//		}
+//	}
+//}
+
+
+using Microsoft.EntityFrameworkCore;
+using userService;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+}
+
